b70696a5af8acef76d33f4ec0d0539c4ece6f39b
Emily <egp10990@yahoo.com>
2023-05-06 16:05:01 -0500

    Add styling to divs to later manipulate into rows and columns

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var container = document.getElementById("container");
 for (var i= 0; i < 257; i++) {
-    container.innerHTML += '<div class = "box"></div>'
+    container.innerHTML += '<div class = "box"></div>' 
 }
+
