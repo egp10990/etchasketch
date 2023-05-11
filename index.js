660ccfbc48008de92ad7fdc2c88b0aade2df8719
Emily <egp10990@yahoo.com>
2023-05-11 12:09:04 -0500

    Add function for div grid and generate reset button

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var container = document.getElementById("container");
-for (var i= 0; i < 257; i++) {
-    container.innerHTML += '<div class = "box"></div>' 
-}
+generateGrid();
+const button = document.querySelector(".reset");
+button.addEventListener("click", reset);
 
+function generateGrid() {
+  const sketchContainer = document.createElement("div");
+  document.body.appendChild(sketchContainer);
+  sketchContainer.classList.add("sketchContainer");
+
+sketchContainer.addEventListener("mouseover", changeColor);
+}
