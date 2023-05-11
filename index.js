60805954acfb21178754835af7a000bda658ae53
Emily <egp10990@yahoo.com>
2023-05-11 16:53:26 -0500

    Add loop to clone divs

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ function generateGrid() {
   sketchContainer.classList.add("sketchContainer");
 
   sketchContainer.addEventListener("mouseover", changeColor);
+
+  function changeColor(event) {
+    event.target.classList.add("tileHovered");
+    }
+}
+
+const gridTile = document.createElement("div");
+sketchContainer.appendChild(gridTile);
+gridTile.classList.add("gridTile");
+let clonedTile;
+for (let i = 0; i < 16 * 16 - 1; i++) {
+  clonedTile = gridTile.cloneNode();
+  sketchContainer.appendChild(clonedTile);
 }
