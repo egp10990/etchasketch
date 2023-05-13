02679039442f9c2646f7f2b2ec6b60291e5a1513
Emily <egp10990@yahoo.com>
2023-05-13 14:05:51 -0500

    Set constant variables for buttons and forms

---
+++ index.js
@@ -0,0 +1,11 @@
+const container = document.getElementsByClassName("container");
+const clear = document.getElementsByClassName("clear");
+const gridSize = document.getElementsByClassName("gridsize");
+const submit = document.getElementsByClassName("submit");
+const black = document.getElementsByClassName("black");
+const rainbow = document.getElementsByClassName("rainbow");
+
+function defaultGrid() {
+for (let i=0; i < 16 * 16; i++);
+
+}
